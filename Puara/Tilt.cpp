5280c3fc5b26ccd5a76db72ca5e03fbe2e5a38b5
yashtiwari9182 <yrevash@Yashs-MacBook-Air.local>
2025-06-05 17:58:33 +0530

    feat: Add Puara Tilt gesture processor

--- Puara/Tilt.cpp
+++ Puara/Tilt.cpp
@@ -1,8 +1,26 @@
 #include "Tilt.hpp"
+
 namespace puara_gestures::objects
 {
 void Tilt::operator()(halp::tick t)
 {
-  outputs.output = impl.tilt(inputs.accel, inputs.gyro, inputs.mag, 0.1);
+  //1. calcutating the processing period --> duraring of this->tick in sec
+  double period_s = 0.0 ;
+  if(setup.rate>0 && t.frames>0) {//as learnt setup.rate is samples per second
+    period_s = static_cast<double>(t.frames/setup.rate);
+  }else{}
+
+  //2, getting input data
+  const puara_gestures::Coord3D &current_accel = inputs.accel.value;
+  const puara_gestures::Coord3D &current_gyro = inputs.gyro.value;
+  const puara_gestures::Coord3D &current_mag = inputs.mag.value;
+
+  //3. calling puara tilt impl process method
+  double tilt_value_rad = impl.tilt(current_accel,current_gyro,current_mag,period_s);
+
+  //4 setting the output port value
+  // also to note the output value is float and the tilt_value_rat is a double
+  outputs.output.value = static_cast<float>(tilt_value_rad);
+
 }
 }
