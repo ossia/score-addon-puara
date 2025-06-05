5dc8b736b1693fdf5e2bb8d41d37eb79d3210d59
yashtiwari9182 <yrevash@Yashs-MacBook-Air.local>
2025-06-05 20:36:22 +0530

    Fix: Update Jab processor to output Coord3D per-axis values

--- Puara/Jab.cpp
+++ Puara/Jab.cpp
@@ -1,42 +1,21 @@
 #include "Jab.hpp"
 namespace puara_gestures::objects
 {
-void Jab::operator()(halp::tick t) // for my info t contain info like t.frames
+void Jab::operator()(halp::tick t)
 {
-  // get input data (avendish)
 
-  const puara_gestures::Coord3D & current_accel = inputs.accel.value;
-  printf("Accel: x=%.2f, y=%.2f, z=%.2f\n", current_accel.x, current_accel.y, current_accel.z);
-
-
-  //2.updating the  puara gestues implementation
-  // imlp is now type Jab3D with method of update that takes x,y,z components as referred from puara/descriptors/jab.h
-
+  //1.  input
+  const puara_gestures::Coord3D& current_accel = inputs.accel.value;
+  //2. calling jab 3d impl.update() -->leading to updating the  internel x,y,z jab objects within 'impl'
   impl.update(current_accel.x, current_accel.y, current_accel.z);
 
+  //3. getting the  calculated jab vecotr
+  puara_gestures::Coord3D calculated_jab_vector = impl.current_value();
 
-  //3. getting the processed value from puara
-  // as learned impl.current_value() for jab3d return coord3d representing jab intensities on each axis
-  puara_gestures::Coord3D jab_vector = impl.current_value();
-  printf("Puara Jab Vector: x=%.2f, y=%.2f, z=%.2f\n", jab_vector.x, jab_vector.y, jab_vector.z);
+  //4.setting the output port
 
-  //4. calculating the desired single fload value / magnitude = sqrt(x^2 + y^2 + z^2)
-
-  float jab_magnitude = std::sqrt(
-      (jab_vector.x * jab_vector.x) +
-      (jab_vector.y * jab_vector.y) +
-      (jab_vector.z * jab_vector.z));
-
-  //5. setting output float value
-
-  outputs.output.value = jab_magnitude;
-  printf("Calculated Jab Magnitude: %.2f\n", jab_magnitude);
+  outputs.output.value = calculated_jab_vector;
 
 
-
-
-
-  // TODO
-  // outputs.output = impl.jab(inputs.accel, inputs.gyro, inputs.mag, 0.1);
 }
 }
