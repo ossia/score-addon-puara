3356c506d13d7e672e60272635dc4abb7ed0c9de
yashtiwari9182 <yrevash@Yashs-MacBook-Air.local>
2025-06-07 00:39:29 +0530

    clang-format , check for setup.rate t.frames to prevent issues with period calculation, and simplified the impl.tilt call.

--- Puara/Tilt.cpp
+++ Puara/Tilt.cpp
@@ -4,23 +4,20 @@ namespace puara_gestures::objects
 {
 void Tilt::operator()(halp::tick t)
 {
-  //1. calcutating the processing period --> duraring of this->tick in sec
-  double period_s = 0.0 ;
-  if(setup.rate>0 && t.frames>0) {//as learnt setup.rate is samples per second
-    period_s = static_cast<double>(t.frames/setup.rate);
-  }else{}
 
-  //2, getting input data
-  const puara_gestures::Coord3D &current_accel = inputs.accel.value;
-  const puara_gestures::Coord3D &current_gyro = inputs.gyro.value;
-  const puara_gestures::Coord3D &current_mag = inputs.mag.value;
+  if(setup.rate <= 0.0 || t.frames <= 0)
+  {
+    return;
+  }
 
-  //3. calling puara tilt impl process method
-  double tilt_value_rad = impl.tilt(current_accel,current_gyro,current_mag,period_s);
+  // Casting t.frames to double ensures floating-point division.
+  const double period_s = static_cast<double>(t.frames) / setup.rate;
 
-  //4 setting the output port value
-  // also to note the output value is float and the tilt_value_rat is a double
-  outputs.output.value = static_cast<float>(tilt_value_rad);
+  // Calling puara_gestures::Tilt::tilt method with direct inputs.
+  const double tilt_value_rad
+      = impl.tilt(inputs.accel.value, inputs.gyro.value, inputs.mag.value, period_s);
 
+  // Setting the output port value.
+  outputs.output.value = tilt_value_rad;
 }
 }
