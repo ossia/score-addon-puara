4eb9a3e134637e7fd5bb7870e4d5c14933185e5d
yashtiwari9182 <yrevash@Yashs-MacBook-Air.local>
2025-06-09 11:31:30 +0530

    CleanUp with removal of static cast

--- Puara/LeakyIntegratorAvnd.cpp
+++ Puara/LeakyIntegratorAvnd.cpp
@@ -9,15 +9,13 @@ void LeakyIntegratorAvnd::operator()(halp::tick t)
   const float current_leak_factor = inputs.leak_param.value;
   const float current_leak_frequency_hz = inputs.leak_frequency.value;
 
-  const int desired_frequency_int = static_cast<int>(current_leak_frequency_hz);
+  const int desired_frequency_int = current_leak_frequency_hz;
   if(impl.frequency != desired_frequency_int)
   {
     impl.frequency = desired_frequency_int;
   }
 
-  const double output_val = impl.integrate(
-      static_cast<double>(current_input_signal),
-      static_cast<double>(current_leak_factor));
+  const double output_val = impl.integrate(current_input_signal, current_leak_factor);
 
   outputs.output_value.value = output_val;
 }
