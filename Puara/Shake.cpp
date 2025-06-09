d8a45d460bffbea5e8013be06c38a9e0bff65513
Yash Gyanendra Tiwari <yashtiwari9182@gmail.com>
2025-06-09 21:06:39 +0530

    feat: Add Puara Roll gesture processor (#6)
    * feat: Add Puara Roll gesture processor
    
    * Change of UUID
    
    * Added Roll Description with url to puara repo
    
    * Added description and url
    
    * clang-formatted & freqency log mapper implemented
    
    * Delete Puara/Shake.hpp
    
    * Delete Puara/Shake.cpp
    
    * Required Changes with clang-format and cleanup of .cpp file as per mentor feedback on tilt
    
    * removed M_pi, performed other conversions
    
    ---------
    
    Co-authored-by: yashtiwari9182 <yrevash@Yashs-MacBook-Air.local>

--- Puara/Shake.cpp
+++
@@ -1,9 +0,0 @@
-#include "Shake.hpp"
-namespace puara_gestures::objects
-{
-void Shake::operator()(halp::tick t)
-{
-  // TODO
-  // outputs.output = impl.shake(inputs.accel, inputs.gyro, inputs.mag, 0.1);
-}
-}
