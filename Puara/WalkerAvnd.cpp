996a3dd4b0fee199cb9a2d41a6e9d66573d242e3
yashtiwari9182 <yrevash@Yashs-MacBook-Air.local>
2025-06-22 19:06:17 +0530

    feat(simulation): Add Walker processor

---
+++ Puara/WalkerAvnd.cpp
@@ -0,0 +1,54 @@
+#include "WalkerAvnd.hpp"
+
+namespace puara_gestures::objects
+{
+
+WalkerAvnd::WalkerAvnd()
+{
+  reset_position();
+}
+
+void WalkerAvnd::operator()()
+{
+  if(!params.reset.value.has_value())
+  {
+    reset_position();
+  }
+
+  double angle_deg = inputs.angle.value;
+  double velocity = inputs.velocity.value;
+  bool is_on_water = inputs.on_water.value;
+
+  if(is_on_water)
+  {
+    velocity *= params.water_speed_factor.value;
+  }
+
+  const double angle_rad = angle_deg * ossia::pi / 180.0;
+  m_current_latitude += velocity * std::cos(angle_rad);
+  m_current_longitude += velocity * std::sin(angle_rad);
+
+  if(m_current_latitude > 90.0)
+  {
+    m_current_latitude = 180.0 - m_current_latitude;
+  }
+  else if(m_current_latitude < -90.0)
+  {
+    m_current_latitude = -180.0 - m_current_latitude;
+  }
+
+  m_current_longitude = std::fmod(m_current_longitude + 180.0, 360.0) - 180.0;
+  if(m_current_longitude == -180.0)
+    m_current_longitude = 180.0;
+
+  outputs.latitude.value = m_current_latitude;
+  outputs.longitude.value = m_current_longitude;
+}
+
+void WalkerAvnd::reset_position()
+{
+  m_current_latitude = params.initial_latitude.value;
+  m_current_longitude = params.initial_longitude.value;
+}
+
+}
