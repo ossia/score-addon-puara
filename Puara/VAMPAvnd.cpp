4ba38f8a3ade84e25b5400bfb986b311b2871415
pyandcpp-coder <yashtiwari9182@gmail.com>
2025-07-29 23:07:50 +0530

    Improved Vamp

--- Puara/VAMPAvnd.cpp
+++ Puara/VAMPAvnd.cpp
@@ -5,67 +5,108 @@ namespace puara_gestures::objects
 
 VAMPAvnd::VAMPAvnd()
 {
+  m_current_epoch = xt::xarray<double>::from_shape({0, 0});
   refit_model();
 }
 
 void VAMPAvnd::operator()()
 {
-  if(!params.reset.value.has_value())
+  if(inputs.reset.value.has_value())
   {
     reset_state();
   }
-  if(params.time_lag.value != m_model->lag() || params.n_dims.value != m_model->dims())
+
+  if(inputs.time_lag.value != m_model->lag() || inputs.n_dims.value != m_model->dims())
   {
     refit_model();
   }
 
   const auto& input_vec = inputs.data.value;
-  const int n_channels = params.n_channels.value;
+  const int n_channels = inputs.n_channels.value;
+
   if(input_vec.empty() || input_vec.size() % n_channels != 0)
   {
     outputs.comps.value.clear();
     return;
   }
+
   size_t n_samples = input_vec.size() / n_channels;
   auto input_arr = xt::adapt(
       input_vec.data(), input_vec.size(), xt::no_ownership(),
       std::vector<size_t>{n_samples, (size_t)n_channels});
 
-  if(params.collect.value)
+  if(inputs.collect.value)
   {
-    if(m_current_epoch.shape()[0] == 0)
+    if(m_current_epoch.size() == 0 || m_current_epoch.dimension() == 0)
     {
       m_current_epoch = input_arr;
     }
     else
     {
-      m_current_epoch = xt::concatenate(xt::xtuple(m_current_epoch, input_arr), 0);
+      // Ensure both arrays have the same number of dimensions and channels
+      if(m_current_epoch.shape()[1] == input_arr.shape()[1])
+      {
+        m_current_epoch = xt::concatenate(xt::xtuple(m_current_epoch, input_arr), 0);
+      }
+      else
+      {
+        // If channel count mismatch, restart with new data
+        m_current_epoch = input_arr;
+      }
     }
 
-    const int target_epoch_size = params.epoch_size.value + params.time_lag.value;
+    const int target_epoch_size = inputs.epoch_size.value + inputs.time_lag.value;
     if(m_current_epoch.shape()[0] >= target_epoch_size)
     {
-      m_buffer.push_back(m_current_epoch);
-      m_model->partial_fit(m_current_epoch);
-      m_model->solve();
-      m_current_epoch.resize({0, (size_t)n_channels});
+      // Take only the required amount of data
+      auto epoch_data
+          = xt::view(m_current_epoch, xt::range(0, target_epoch_size), xt::all());
+      xt::xarray<double> epoch_copy = epoch_data;
+
+      m_buffer.push_back(epoch_copy);
+      m_model->partial_fit(epoch_copy);
+
+      // Only solve if we have enough data
+      if(m_buffer.size() >= 2) // Need at least 2 epochs for meaningful VAMP
+      {
+        m_model->solve();
+      }
+      if(m_current_epoch.shape()[0] > target_epoch_size)
+      {
+        auto remaining = xt::view(
+            m_current_epoch, xt::range(target_epoch_size, m_current_epoch.shape()[0]),
+            xt::all());
+        m_current_epoch = remaining;
+      }
+      else
+      {
+        m_current_epoch = xt::xarray<double>::from_shape({0, (size_t)n_channels});
+      }
     }
   }
+
   if(m_model->is_fitted())
   {
     auto comps_arr = m_model->transform(input_arr);
-    outputs.comps.value.assign(comps_arr.begin(), comps_arr.end());
+    if(comps_arr.size() > 0)
+    {
+      outputs.comps.value.assign(comps_arr.begin(), comps_arr.end());
+    }
+    else
+    {
+      outputs.comps.value.clear();
+    }
   }
   else
   {
-    outputs.comps.value.clear();
+    outputs.comps.value.assign(n_samples * inputs.n_dims.value, 0.0);
   }
 }
 
 void VAMPAvnd::refit_model()
 {
   m_model = std::make_unique<algorithms::VampModel>(
-      params.time_lag.value, params.n_dims.value);
+      inputs.time_lag.value, inputs.n_dims.value);
   for(const auto& epoch : m_buffer)
   {
     m_model->partial_fit(epoch);
@@ -79,7 +120,8 @@ void VAMPAvnd::refit_model()
 void VAMPAvnd::reset_state()
 {
   m_buffer.clear();
-  m_current_epoch.resize({0, (size_t)params.n_channels.value});
+  const int n_channels = inputs.n_channels.value;
+  m_current_epoch = xt::xarray<double>::from_shape({0, (size_t)n_channels});
   m_model->reset();
 }
 
--- Puara/VAMPAvnd.hpp
+++ Puara/VAMPAvnd.hpp
@@ -1,7 +1,9 @@
 #pragma once
 #include "vamp_algorithms.hpp"
+
 #include <halp/controls.hpp>
 #include <halp/meta.hpp>
+
 #include <vector>
 
 namespace puara_gestures::objects
@@ -16,25 +18,21 @@ public:
   halp_meta(description, "Finds slowest components in a time-series using VAMP.")
   halp_meta(uuid, "3ebf910a-330f-43d8-8958-287b6f191c22")
 
-  struct ins
+  struct
   {
     halp::val_port<"Data", std::vector<double>> data;
-  } inputs;
-
-  struct outs
-  {
-    halp::val_port<"Components", std::vector<double>> comps;
-  } outputs;
-
-  struct parameters
-  {
     halp::knob_i32<"Num Channels", halp::range{1, 64, 2}> n_channels;
     halp::knob_i32<"Time Lag (samples)", halp::range{1, 300, 10}> time_lag;
     halp::knob_i32<"Num Dimensions", halp::range{1, 8, 2}> n_dims;
     halp::knob_i32<"Epoch Size (samples)", halp::range{2, 1000, 256}> epoch_size;
     halp::toggle<"Collect Data", halp::toggle_setup{true}> collect;
     halp::impulse_button<"Reset Model"> reset;
-  } params;
+  } inputs;
+
+  struct
+  {
+    halp::val_port<"Components", std::vector<double>> comps;
+  } outputs;
 
   VAMPAvnd();
   void operator()();
